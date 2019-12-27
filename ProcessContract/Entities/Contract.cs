144202c988ddadcd64dcc346bc99bd0dc2545536
Ruan Felipe <ruanfelipedpontes@gmail.com>
2019-12-27 14:08:04 -0300

    Entities adicionadas

---
+++ ProcessContract/Entities/Contract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessContract.Entities
+{
+    class Contract
+    {
+        public int number { get; set; }
+        public DateTime date { get; set; }
+        public double totalValue { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            this.number = number;
+            this.date = date;
+            this.totalValue = totalValue;
+        }
+    }
+}
---
+++ ProcessContract/Entities/Installment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessContract.Entities
+{
+    class Installment
+    {
+        public DateTime dueDate { get; private set; }
+        public double amount { get; private set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            this.dueDate = dueDate;
+            this.amount = amount;
+        }
+    }
+}
